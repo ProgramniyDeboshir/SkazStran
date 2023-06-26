832ce8cdf206e81507d0e3b035e7a17584d787d3
ProgramniyDeboshir <137521774+ProgramniyDeboshir@users.noreply.github.com>
2023-06-26 16:16:54 +0800

    Добавьте файлы проекта.

---
+++ SkazStran/Pages/Autorization.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SkazStran.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        SkazStranEntities context;
+        public Authorization(SkazStranEntities cont)
+        {
+            InitializeComponent();
+            context = cont;
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 30);
+            timer.Tick += Timer_Tick;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Enter.IsEnabled = true;
+            timer.Stop();
+        }
+
+        DispatcherTimer timer;
+        int countClick = 0;
+        private void EnterClick(object sender, RoutedEventArgs e)
+        {
+            countClick++;
+            string log = LoginBox.Text;
+            string pass = PasswordBox.Password;
+            User user = context.User.Find(log);
+            if (user != null)
+            {
+                if (user.Password.Equals(pass))
+                {
+                    MessageBox.Show("Вы успешно авторизованы!");
+                    countClick = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Ввведен неверный пароль!");
+                    if (countClick >= 3)
+                    {
+                        RemPass.Visibility = Visibility.Visible;
+                        timer.Start();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Такого пользователя не существует!");
+                if (countClick >= 3)
+                {
+                    Enter.IsEnabled = false;
+                    timer.Start();
+                }
+            }
+        }
+
+        private void ResPassClick(object sender, RoutedEventArgs e)
+        {
+            Remember remWindow = new Remember(context);
+            remWindow.Show();
+        }
+    }
+}
---
+++ SkazStran/Pages/Remember.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SkazStran.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Remember.xaml
+    /// </summary>
+    public partial class Remember : Window
+    {
+        SkazStranEntities context;
+        public Remember(SkazStranEntities cont)
+        {
+            InitializeComponent();
+            context = cont;
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ShowPassClick(object sender, RoutedEventArgs e)
+        {
+            int tab1 =Convert.ToInt32(tab.Text);
+            string fio1 = fio.Text;
+            DateTime dat1 =Convert.ToDateTime(dat.Text);
+            string pos1 = pos.Text;
+            User user= context.User.Find(fio1);
+            if (user != null)
+            {
+                if (user.tabNum.Equals(tab1))
+                {
+                    if (user.position.Equals(pos1))
+                    {
+                        if (user.dateStartWork.Equals(dat1))
+                        {
+                            MessageBox.Show("Ваш Пароль: " + user.Password);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
